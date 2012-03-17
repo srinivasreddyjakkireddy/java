22839a36ef92e52b1a7af9c6a6440179fc267f74
srinivasreddyjakkireddy <srinu_1003@yahoo.com>
2012-03-17 19:17:08 +0530

    Find the highest value in an array

---
+++ Array2h.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Array2
+ {
+    public static void main(String []args)
+     {
+int largest=0;
+        int a[]=new int[5];
+       System.out.println("Enter array values");
+       Scanner s=new Scanner(System.in);
+        for(int i=0;i<5;i++)
+           {
+             a[i]=s.nextInt();
+           }
+for(int j=0;j<=4;j++)
+ {
+     if(a[j]>largest)
+         {
+            largest=a[j];
+         }
+    }
+System.out.print(largest);
+            
+           
+       }
+ }
+        
+              
+        
